1b32eb9e7f982e69b38b43c58345e7ee2ba14af0
John Wallace <jnwywe@gmail.com>
2018-05-14 14:19:51 -0500

    @supressswarning fix

--- src/Author.java
+++ src/Author.java
@@ -4,6 +4,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Author extends Person {
     private List books;
 
@@ -15,7 +16,7 @@ public class Author extends Person {
     /**
      * @deprecated Use publishedBooks instead
      */
-    @Deprecated
+    @SuppressWarnings("unchecked")
     public List<String> getBooks() {
         return books;
     }
